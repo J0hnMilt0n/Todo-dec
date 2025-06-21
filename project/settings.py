f1f09b5f37c03a6f94f8616731f6fdf255a53271
J0hnMilt0n <mjohnmilton41@outlook.com>
2025-06-21 11:06:57 +0530

    added server config

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-65vcm@z+an(915#*vua%*@_m=i43kaqfi=s2zsa!0m7_(97joy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
